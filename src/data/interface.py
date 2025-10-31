3fb6b08a4696f38e3ae31b8ac2e088f4ddc802b8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-31 23:55:09 +0000

    Update src/data/interface.py: In Progress

--- src/data/interface.py
+++ src/data/interface.py
@@ -6,9 +6,11 @@ import pandas as pd
 import src.data.codes
 import src.data.reference
 import src.data.menu
-import src.elements.specifications as se
 import src.elements.s3_parameters as s3p
+import src.elements.service as sr
+import src.elements.specification as sc
 import src.data.specifications
+import src.data.cases
 
 
 class Interface:
@@ -16,13 +18,14 @@ class Interface:
     Interface
     """
 
-    def __init__(self, s3_parameters: s3p.S3Parameters):
+    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters):
         """
 
         :param s3_parameters: The overarching S3 (Simple Storage Service) parameters
                               settings of this project, e.g., region code name, buckets, etc.
         """
 
+        self.__service: sr.Service = service
         self.__s3_parameters: s3p.S3Parameters = s3_parameters
 
         # Logging
@@ -31,23 +34,24 @@ class Interface:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def exc(self) -> list[se.Specifications]:
+    def exc(self):
         """
 
         :return:
         """
 
-        _codes: list[str] = src.data.codes.Codes().exc()
-        codes = [int(os.path.split(code)[-1]) for code in _codes]
-        self.__logger.info(codes)
+        cases = src.data.cases.Cases(service=self.__service, s3_parameters=self.__s3_parameters).exc()
+        self.__logger.info(cases)
 
+        # Reference
         reference: pd.DataFrame = src.data.reference.Reference(
-            s3_parameters=self.__s3_parameters).exc(codes=codes)
+            s3_parameters=self.__s3_parameters).exc(codes=cases['ts_id'].unique())
+        reference = reference.copy().merge(cases, how='left', on=['catchment_id', 'ts_id'])
         self.__logger.info(reference)
 
+        # Menu
         src.data.menu.Menu().exc(reference=reference)
 
-        specifications_: list[se.Specifications] = src.data.specifications.Specifications().exc(
-            reference=reference)
-
-        return specifications_
+        # Specifications
+        specifications: list[sc.Specification] = src.data.specifications.Specifications().exc(reference=reference)
+        self.__logger.info(specifications)
