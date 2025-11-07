714423d185544b55c262b25d8deb16321fa471d1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-11-07 22:32:27 +0000

    Update src/elements/structures.py

--- src/elements/structures.py
+++ src/elements/structures.py
@@ -12,7 +12,11 @@ class Structures(typing.NamedTuple):
     ----------<br>
     training<br>
     testing<br>
+    q_training<br>
+    q_testing<br>
     """
 
     training: pd.DataFrame
     testing: pd.DataFrame
+    q_training: pd.DataFrame
+    q_testing: pd.DataFrame
