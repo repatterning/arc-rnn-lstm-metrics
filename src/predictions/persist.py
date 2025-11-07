b148c859b57a8c2fdc02298e169bee4ec7f0a86c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-11-07 23:45:30 +0000

    Add src/predictions/persist.py

---
+++ src/predictions/persist.py
@@ -0,0 +1,71 @@
+"""Module persist.py"""
+import json
+import os
+
+import pandas as pd
+
+import config
+import src.elements.structures as st
+import src.elements.specification as sc
+import src.functions.objects
+
+
+class Persist:
+    """
+    Persist
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+        self.__configurations = config.Config()
+
+        # An instance for writing JSON objects
+        self.__objects = src.functions.objects.Objects()
+
+    @staticmethod
+    def __get_node(blob: pd.DataFrame) -> dict:
+        """
+
+        :param blob:
+        :return:
+        """
+
+        string: str = blob.to_json(orient='split')
+
+        return json.loads(string)
+
+    def __persist(self, nodes: dict, path: str) -> str:
+        """
+
+        :param nodes: Dictionary of data.
+        :param path: ...
+        :return:
+        """
+
+        return self.__objects.write(nodes=nodes, path=path)
+
+    def disaggregates(self, specification: sc.Specification, structures: st.Structures) -> str:
+        """
+
+        :param specification: <br>
+        :param structures: <br>
+        :return:
+        """
+
+        nodes = {
+            'training': self.__get_node(structures.training),
+            'testing': self.__get_node(structures.testing),
+            'q_training': self.__get_node(structures.q_training),
+            'q_testing': self.__get_node(structures.q_testing)
+        }
+        nodes.update(specification._asdict())
+
+        path = os.path.join(self.__configurations.points_, f'{str(specification.ts_id)}.json')
+
+        return self.__persist(nodes=nodes, path=path)
+
+    def aggregates(self):
+        pass
